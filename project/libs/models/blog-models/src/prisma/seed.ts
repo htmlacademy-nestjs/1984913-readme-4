b56be20b2c278cc98041400bf3256833be5c656a
Anastasia <dzheyn.anna@mail.ru>
2023-08-08 00:59:55 +0400

    add seed and db commands

---
+++ project/libs/models/blog-models/src/prisma/seed.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function fillDb() {
+  await prisma.publication.upsert({
+    where: { postId: 1 },
+    update: {},
+    create: {
+      userId:'1',
+      type: "text",
+      title: "Title test",
+      announcement: "test test",
+      text: "Long text",
+      status: 'posted',
+      likesCount:0,
+      commentsCount:0
+    }
+  });
+  await prisma.publication.upsert({
+    where: { postId: 2 },
+    update: {},
+    create: {
+      userId:'1',
+      type: "text",
+      title: "Title test 2",
+      announcement: "Announcement",
+      text: "Long text for publication 2",
+      status: 'posted',
+      likesCount:0,
+      commentsCount:0
+    }
+  });
+
+  console.info('Database was filled')
+}
+
+fillDb()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect()
+
+    process.exit(1);
+  })
